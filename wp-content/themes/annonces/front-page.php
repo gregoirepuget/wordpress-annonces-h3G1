4e2d96c9793c3b13fd179bb63259c6f16922ad5f
gregoirepuget <gregoirepuget@gmail.com>
2016-10-13 17:07:07 +0200

    ajout support thumbnail

---
+++ wp-content/themes/annonces/front-page.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+<section id="main" class="container">
+  <h1>Accueil : <?php bloginfo("title"); ?></h1>
+  <h2><?php bloginfo("description") ; ?></h2>
+  
+</section>
+<?php get_footer(); ?>
--- wp-content/themes/annonces/functions.php
+++ wp-content/themes/annonces/functions.php
@@ -115,6 +115,29 @@ wp_enqueue_style( 'main-style' );
 add_action( 'wp_enqueue_scripts', 'ajout_scripts' );
 
 
+add_action( 'after_setup_theme', 'thumbnails_theme_support' );
+function thumbnails_theme_support(){
+    add_theme_support( 'post-thumbnails' );
+}
+
+add_action( 'after_setup_theme', 'images_sizes' );
+function images_sizes()
+{
+add_image_size("thumbnail_annonce_small",600,450,true);
+add_image_size("thumbnail_annonce_full",1600,600,false);
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
